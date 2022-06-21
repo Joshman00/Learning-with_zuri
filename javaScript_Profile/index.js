92fdee738d92b92fe04c03f8d3055370b64193c2
Joshman <mamodujoshua@gmail.com>
2022-06-22 00:15:38 +0100

    js profile

---
+++ javaScript_Profile/index.js
@@ -0,0 +1,15 @@
+let me = (`Name="Joshua",
+height= "1.75cm",
+Country="Nigeria"`)
+
+alert(me)
+
+// let Joshua{
+//     Name = "Joshua",
+//     Height = "1.70cm",
+//     country = "Nigeria",
+// }
+
+// console.log (Joshua)
+
+// alert ("Hello Josh")
