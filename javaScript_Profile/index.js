94c1f703f32089c32e36c7ced6215c3f0dadbe1f
Joshman <93885279+Joshman00@users.noreply.github.com>
2022-06-24 22:57:00 +0100

    index.js

--- javaScript_Profile/index.js
+++ javaScript_Profile/index.js
@@ -3,20 +3,3 @@ height= "1.75cm",
 Country="Nigeria"`)
 
 alert(me)
-
-// let Joshua{
-//     Name = "Joshua",
-//     Height = "1.70cm",
-//     country = "Nigeria",
-// }
-
-// console.log (Joshua)
-
-// alert ("Hello Josh")
-
-
-// let result = 0;
-// for (let i = 0; i < 5; i++) {
-//     result += i;
-// }
-// console.log(result);
